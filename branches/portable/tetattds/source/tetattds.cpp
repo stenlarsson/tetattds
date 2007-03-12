4448135b0981676cac97c8fd69d9200d305b8d13
grddev <grddev@c5b8b1e8-4a23-0410-8942-0b8bfcdd0bd3>
2007-03-12 19:54:00 +0000

    Moved platform-specific parts in tetattds.cpp into a main.cpp

--- branches/portable/tetattds/source/tetattds.cpp
+++ branches/portable/tetattds/source/tetattds.cpp
@@ -1,71 +1,34 @@
 #include "tetattds.h"
 
 #include <stdio.h>
-#include <nds/arm9/console.h>
-
-#include <fat.h>
-#include <dswifi9.h>
 
 #include <driver.h>
-#include <theme.h>
-
-#include "menubackground_bin.h"
 
 #include "singlebackground_bin.h"
 #include "singlebackground_map_bin.h"
 #include "singlebackground_pal_bin.h"
 
-#include "splash2_bin.h"
-#include "splash2_map_bin.h"
-#include "splash2_pal_bin.h"
-
-#include "menu1_bin.h"
-#include "menu2_bin.h"
-
 #include "game.h"
-#include "wifi.h"
 #include "mainmenudialog.h"
 #include "leveldialog.h"
 #include "wifimenudialog.h"
 #include <textentrydialog.h>
-#include "fieldgraphics.h"
-#include "util.h"
 #include "settings.h"
-#include "sound.h"
-#include "fifo.h"
-#include "playfield.h"
 #include "statusdialog.h"
 
 #include "serverconnection.h"
 #include "connectionmanager.h"
+#include "state.h"
 
-#define BLEND_SRC_ALL 0x003F
+extern unsigned int currentTime;
 
-class State {
-public:
-	virtual ~State() {}
-	virtual void Enter() = 0;
-	virtual void Tick() = 0;
-	virtual void Exit() = 0;
-};
-
-static unsigned int currentTime = 0;
-
-static bool hasSetupWifi = false;
-static bool supportsFat = false;
 static ConnectionManager* connectionManager = NULL;
 static ServerConnection* connection = NULL;
 
 static int level = 4;
-static char name[10];
-static Settings* settings = NULL;
-static FwGui::Driver* gui = NULL;
-
-// used in network lib
-unsigned int GetTime()
-{
-	return currentTime/60;
-}
+extern char name[10];
+extern Settings* settings;
+extern FwGui::Driver* gui;
 
 State* highscoreState;
 State* mainMenuState;
@@ -75,17 +38,17 @@ State* endlessGameState;
 State* vsSelfGameState;
 State* wifiState;
 State* wifiHostEntryState;
-State* setupWifiState;
+extern State* setupWifiState;
 State* wifiMenuState;
 State* wifiLevelState;
 State* wifiChatEntryState;
 State* wifiGameState;
-State* splashScreenState;
+extern State* splashScreenState;
 State* waitForServerAcceptState;
 State* waitForGameEndState;
 
-static State* currentState = NULL;
-static State* nextState = NULL;
+State* currentState = NULL;
+State* nextState = NULL;
 
 class HighscoreState : public State {
 public:
@@ -482,66 +445,6 @@ private:
 	State* acceptState;
 };
 
-class SetupWifiState : public State {
-public:
-	SetupWifiState() 
-		: dialog(NULL) {
-	}
-	virtual void Enter() {
-		FieldGraphics::InitSubScreen(true);
-		g_fieldGraphics->ClearChat();
-
-		dialog = new StatusDialog("PLEASE WAIT");
-		gui->SetActiveDialog(dialog);
-		if(hasSetupWifi) {
-			nextState = wifiState;
-			return;
-		}
-		SetupWifi();
-		status = ASSOCSTATUS_DISCONNECTED;
-	}
-	virtual void Tick() {
-		if (dialog->abort) {
-			nextState = mainMenuState;
-			return;
-		}
-		
-		WIFI_ASSOCSTATUS newStatus = (WIFI_ASSOCSTATUS)Wifi_AssocStatus();
-		if(status != newStatus) {
-			status = newStatus;
-			static const char* statusMessages[] = {
-				"Disconnected",
-				"Searching...",
-				"Authenticating...",
-				"Associating...",
-				"Acquiring DHCP...",
-				"Associated",
-				"Cannot connect"
-			};
-			char statusString[1024];
-			sprintf(
-				statusString,
-				"Connecting to Access Point\n%s",
-				(status <= 6) ? statusMessages[status] : "???");
-			dialog->SetStatus(statusString);
-				
-			if(status == ASSOCSTATUS_ASSOCIATED) {
-				hasSetupWifi = true;
-				nextState = wifiState;
-			}
-		}
-	}
-
-	virtual void Exit() {
-		gui->SetActiveDialog(NULL);
-		delete dialog;
-		dialog = NULL;
-	}
-private:
-	StatusDialog* dialog;
-	WIFI_ASSOCSTATUS status;
-};
-
 class WifiState : public State {
 	virtual void Enter() {
 		currentSubState = NULL;
@@ -598,95 +501,6 @@ private:
 	State * currentSubState;
 };
 
-class SplashScreenState : public State {
-public:
-	virtual void Enter() {
-		swiWaitForVBlank();
-		lcdMainOnBottom();
-		
-		// main screen
-		videoSetMode(
-			MODE_0_2D |
-			DISPLAY_BG0_ACTIVE);
-		// 128K
-		vramSetBankA(VRAM_A_MAIN_BG);
-
-		// palette
-		Decompress(BG_PALETTE, splash2_pal_bin);
-	
-		// splashscreen
-		BG0_CR =
-			BG_32x32 |
-			BG_TILE_BASE(BACKGROUND_TILE_BASE) |
-			BG_MAP_BASE(BACKGROUND_MAP_BASE) |
-			BG_PRIORITY(BACKGROUND_PRIORITY) |
-			BG_256_COLOR;
-		Decompress((void*)BG_TILE_RAM(BACKGROUND_TILE_BASE), splash2_bin);
-		Decompress((void*)BG_MAP_RAM(BACKGROUND_MAP_BASE), splash2_map_bin);
-		
-		// fade in
-		for(int i = 31; i >= 0; i--)
-		{
-			BLEND_Y = i>>1;
-			SUB_BLEND_Y = i>>1;
-			swiWaitForVBlank();
-		}
-		
-		// load music
-		Sound::InitMusic();
-		Sound::LoadMusic();
-	}
-	
-	void Tick() {
-		nextState = mainMenuState;
-	}
-	
-	void Exit() {
-		// fade out
-		BLEND_CR = BLEND_FADE_BLACK | BLEND_SRC_ALL;
-		SUB_BLEND_CR = BLEND_FADE_BLACK | BLEND_SRC_ALL;
-		
-		for(int i = 0; i < 32; i++)
-		{
-			BLEND_Y = i>>1;
-			SUB_BLEND_Y = i>>1;
-			swiWaitForVBlank();
-		}
-		
-		// init fatLib
-		supportsFat = fatInitDefault();
-		
-		if(supportsFat)
-			settings = new FatSettings();
-		else
-			settings = new SramSettings();
-		
-		settings->Load();
-	}
-};
-
-void GetName()
-{
-	memset(name, 0, sizeof(name));
-	char* src = (char*)PersonalData->name;
-	char* dest = name;
-	for(int i = 0; i < PersonalData->nameLen; i++)
-	{
-		*dest = *src;
-		dest++;
-		src+=2;
-	}
-	*dest = '\0';
-}
-
-void myExceptionHandler()
-{
-	for(int i = 0; i < 16; i++)
-		printf("r%02i: 0x%08lx\n", i, exceptionRegisters[i]);
-
-	for(;;);
-}
-
 void InitStates()
 {
 	highscoreState = new HighscoreState;
@@ -694,118 +508,30 @@ void InitStates()
 	endlessGameState = new EndlessGameState;
 	vsSelfGameState = new VsSelfGameState;
 	wifiState = new WifiState;
-	setupWifiState = new SetupWifiState;
 	wifiHostEntryState = new WifiHostEntryState;
 	wifiMenuState = new WifiMenuState;
 	wifiChatEntryState = new WifiChatEntryState;
 	waitForGameEndState = new WaitForServerState(SERVERSTATE_GAME_ENDED, wifiMenuState);
 	wifiGameState = new WifiGameState;
-	splashScreenState = new SplashScreenState;
 	waitForServerAcceptState = new WaitForServerState(SERVERSTATE_ACCEPTED, wifiMenuState);
 	endlessLevelState = new LevelState(endlessGameState, mainMenuState);
 	vsSelfLevelState = new LevelState(vsSelfGameState, mainMenuState);
 	wifiLevelState = new LevelState(wifiMenuState, wifiMenuState);
-}
 
-void InitConsole()
-{
-	BLEND_CR = BLEND_FADE_WHITE | BLEND_SRC_ALL;
-	SUB_BLEND_CR = BLEND_FADE_WHITE | BLEND_SRC_ALL;
-	BLEND_Y = 15;
-	SUB_BLEND_Y = 15;
-	swiWaitForVBlank();
-
-	// sub screen
-	videoSetModeSub(
-		MODE_0_2D |
-		DISPLAY_BG3_ACTIVE);
-	// 128K
-	vramSetBankC(VRAM_C_SUB_BG);
-
-	BG_PALETTE_SUB[0] = RGB15(0,0,0);
-	BG_PALETTE_SUB[255] = RGB15(31,31,31);
-
-	// console
-	SUB_BG3_CR =
-		BG_32x32 |
-		BG_TILE_BASE(CONSOLE_TILE_BASE) |
-		BG_MAP_BASE(CONSOLE_MAP_BASE) |
-		BG_PRIORITY(CONSOLE_PRIORITY) |
-		BG_16_COLOR;
-	consoleInitDefault(
-		(u16*)SCREEN_BASE_BLOCK_SUB(CONSOLE_MAP_BASE),
-		(u16*)CHAR_BASE_BLOCK_SUB(CONSOLE_TILE_BASE),
-		16);
-}
-
-void VBlankHandler()
-{
-	currentTime++;
-}
-
-int main(void)
-{
-	setExceptionHandler(myExceptionHandler);
-	irqInit();
-	irqSet(IRQ_VBLANK, VBlankHandler);
-	irqEnable(IRQ_VBLANK); // needed by swiWaitForVBlank()
-	
-	InitFifo();
-	
-	GetName();
-	
-	InitConsole();
-	Sprite::InitSprites();
-	g_fieldGraphics = new FieldGraphics();
-
-	gui = new FwGui::Driver();
-	int imageSize;
-	u16* image = (u16*)Decompress(menubackground_bin, &imageSize);
-	FwGui::backgroundImage = image;
-	FwGui::selectedColor = FwGui::Color(255, 255, 255);
-	FwGui::labelTextColor = FwGui::Color(0, 0, 0);
-	FwGui::enabledButtonColor = FwGui::Color(0, 0, 0, 190);
-	FwGui::disabledButtonColor = FwGui::Color(171, 171, 171, 190);
-	FwGui::buttonTextColor = FwGui::Color(255, 255, 255);
-	FwGui::enabledEditBoxColor = FwGui::Color(255, 255, 255, 190);
-	TransferSoundData keySound;
-	keySound.data = menu1_bin;
-	keySound.len = menu1_bin_size;
-	keySound.rate = 22050;
-	keySound.vol = 64;
-	keySound.pan = 64;
-	keySound.format = 2;
-	FwGui::keyClickSound = &keySound;
-	
-	TransferSoundData menuSound;
-	menuSound.data = menu2_bin;
-	menuSound.len = menu2_bin_size;
-	menuSound.rate = 22050;
-	menuSound.vol = 64;
-	menuSound.pan = 64;
-	menuSound.format = 2;
-	
-	InitStates();
-	
 	currentState = splashScreenState;
 	currentState->Enter();
-	
-	while(true)
-	{
-		currentState->Tick();
+}
 
-		if (nextState != NULL) {
-			currentState->Exit();
-			currentState = nextState;
-			nextState = NULL;
-			currentState->Enter();
-			playSound(&menuSound);
-		}
+bool StateTick()
+{
+	currentState->Tick();
 
-		gui->Tick();
-		Sound::UpdateMusic();
-		swiWaitForVBlank();
-	}
+	if (nextState == NULL)
+		return false;
 
-	return 0;
+	currentState->Exit();
+	currentState = nextState;
+	nextState = NULL;
+	currentState->Enter();
+	return true;
 }
