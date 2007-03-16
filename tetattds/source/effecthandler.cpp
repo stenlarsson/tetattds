14056ecd77fa26d87cafc3a98c617b2ef321d5bd
Gustav Munkby <grddev@gmail.com>
2007-03-16 10:53:05 +0000

    Changed the effecthandler implementation to use stl::list.

--- tetattds/source/effecthandler.cpp
+++ tetattds/source/effecthandler.cpp
@@ -1,139 +1,48 @@
-#include "tetattds.h"
 #include "effecthandler.h"
 #include "effect.h"
+#include <algorithm>
+#include <functional>
 
+template <typename T>
+static inline void delete_elem(T* & t)
+{
+	delete t;
+	t = NULL;
+}
+
+template <typename It>
+static inline void delete_each(It begin, It end)
+{
+	for (It it = begin; it != end; ++it)
+		delete_elem(*it);
+}
 
 EffectHandler::EffectHandler()
+  : effects()
 {
-	first = NULL;
-	last = NULL;
-	current = NULL;
-	bGet = false;
 }
 
 EffectHandler::~EffectHandler()
 {
-	if(first != NULL)
-	{
-		current = first;
-
-		while(current->next != NULL)
-		{
-			last = current;
-			current = current->next;
-			delete last->effect;
-			delete last;
-		}
-		delete current->effect;
-		delete current;
-	}
+	delete_each(effects.begin(), effects.end());
+	effects.clear();
 }
 
-void EffectHandler::Add(Effect* newEff)
+void EffectHandler::Add(Effect* e)
 {
-
-	EffList* newList;
-
-	newList = new EffList;
-	newList->effect = newEff;
-	newList->next = NULL;
-
-	//check if it's the first layer created
-	if(last == NULL)
-	{
-		first = last = newList;
-		return;
-	}
-
-	//else stick it at the end
-	last->next = newList;
-	last = newList;
-}
-
-void EffectHandler::Remove(Effect* remEff)
-{
-	EffList* temp;
-	EffList* temp2;
-
-	//check if it's the first or 'last'
-	if(first->effect == remEff)
-	{
-		temp2 = first;
-		first = first->next;
-		if(first == NULL)
-			last = NULL;
-		delete temp2->effect;
-		delete temp2;
-		current = first;
-		return;
-	}
-
-	temp = first;
-	//else go look it up
-	while(temp != NULL && temp->effect != remEff)
-	{
-		temp2 = temp;
-		temp = temp->next;
-	}
-	//and get rid of it =)
-	if(temp != NULL)
-	{
-		temp2->next = temp->next;
-		if(temp2->next == NULL)
-			last = temp2;
-		delete temp->effect;
-		delete temp;
-	}
-	current = first;
-}
-
-Effect* EffectHandler::GetEffects()
-{
-	if(first == NULL)
-		return NULL;
-
-	if(!bGet)
-	{
-		current = first;
-		bGet = true;
-		return current->effect;
-	}
-
-	if(current == NULL)
-	{
-		bGet = false;
-		return NULL;
-	}
-
-	current = current->next;
-
-	if(current == NULL)
-	{
-		bGet = false;
-		return NULL;
-	}
-
-	return current->effect;
+	effects.push_back(e);
 }
 
 void EffectHandler::Tick()
 {
-	Effect* eff;
-	while((eff = GetEffects()) != NULL)
-	{
-		eff->Tick();
-	}
-
-	while((eff = GetEffects()) != NULL)
-	{
-		if(eff->IsDone())
-			Remove(eff);
-	}
+	std::for_each(effects.begin(), effects.end(), std::mem_fun(&Effect::Tick));
+	std::list<Effect*>::iterator keep =
+		std::partition(effects.begin(), effects.end(), std::mem_fun(&Effect::IsDone));
+	delete_each(effects.begin(), keep);
+	effects.erase(effects.begin(), keep);
 }
 
 void EffectHandler::Draw()
 {
-	Effect* eff;
-	while((eff = GetEffects()) != NULL)
-		eff->Draw();
+	std::for_each(effects.begin(), effects.end(), std::mem_fun(&Effect::Draw));
 }
--- tetattds/source/effecthandler.h
+++ tetattds/source/effecthandler.h
@@ -1,14 +1,18 @@
 #pragma once
 
+#include <list>
 class Effect;
 
-struct EffList
-{
-  public:
-	Effect* effect;
-	EffList* next;
-};
-
+/**
+ * Keeper of Effect objects.
+ * 
+ * Effects should be allocated with new, and then added to the
+ * handler using Add. The handler assumes ownership and will
+ * delete the Effects as soon as the effect is completed.
+ *
+ * The Tick and Draw methods call the corresponding methods
+ * once for every stored effect object.
+ */
 class EffectHandler
 {
   public:
@@ -20,11 +24,5 @@ class EffectHandler
 	void Draw();
 
   private:
-	Effect* GetEffects();
-	void Remove(Effect* remEff);
-
-	EffList* first;
-	EffList* last;
-	EffList* current;
-	bool bGet;
+	std::list<Effect*> effects;
 };
