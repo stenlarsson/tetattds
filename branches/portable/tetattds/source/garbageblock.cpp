699e9a80e965aa24887a3a8ad2280011e6067980
grddev <grddev@c5b8b1e8-4a23-0410-8942-0b8bfcdd0bd3>
2007-04-01 16:55:28 +0000

    Some refactoring of GarbageBlock

--- branches/portable/tetattds/source/garbageblock.cpp
+++ branches/portable/tetattds/source/garbageblock.cpp
@@ -57,36 +57,25 @@ void GarbageBlock::InitPop(Chain* chain)
 	}
 }
 
+inline static GarbageGraphicsType GetGraphic(int line, int lineCount)
+{      
+  if (lineCount == 1)
+		return GARBAGE_GRAPHICS_SINGLE;
+	else if (line == 0)
+		return GARBAGE_GRAPHICS_TOP;
+	else if (line == lineCount - 1)
+		return GARBAGE_GRAPHICS_BOTTOM;
+	else
+		return GARBAGE_GRAPHICS_MIDDLE;
+}
+
 void GarbageBlock::Pop(int delay, int total, Chain* newchain)
 {
-	if(lines == 2)
-	{
-		chunks[0]->Pop(GARBAGE_GRAPHICS_SINGLE, delay+PF_WIDTH, total);
-//		chunks[0]->SetChain(NULL);
-	}
-	else
-	{
-		for(int i = 0; i < lines-1; i++)
-		{
-			GarbageGraphicsType graphic;
-			if(i == 0)
-			{
-				graphic = GARBAGE_GRAPHICS_TOP;
-			}
-			else if(i == lines - 2)
-			{
-				graphic = GARBAGE_GRAPHICS_BOTTOM;
-			}
-			else
-			{
-				graphic = GARBAGE_GRAPHICS_MIDDLE;
-			}
-			chunks[i]->Pop(graphic, delay+(lines-i-1)*PF_WIDTH, total);
-//			chunks[i]->SetChain(NULL);
-		}
-	}
+	for(int i = 0; i < lines-1; i++)
+		chunks[i]->Pop(
+			GetGraphic(i, lines-1), delay+(lines-i-1)*PF_WIDTH, total);
+
 	chunks[lines-1]->Pop(GARBAGE_GRAPHICS_NONE, delay, total);
-//	chunks[lines-1]->SetChain(newchain);
 
 	state = BST_POP;
 	const LevelData* data = g_game->GetLevelData();
@@ -95,49 +84,18 @@ void GarbageBlock::Pop(int delay, int total, Chain* newchain)
 
 void GarbageBlock::SetGraphic()
 {
-	if(lines == 1)
-	{
-		if(type == GARBAGE_EVIL)
-			chunks[0]->SetGraphic(GARBAGE_GRAPHICS_EVIL);
-		else
-			chunks[0]->SetGraphic(GARBAGE_GRAPHICS_SINGLE);
-	}
+	if(type == GARBAGE_EVIL)
+		chunks[0]->SetGraphic(GARBAGE_GRAPHICS_EVIL);
 	else
-	{
 		for(int i = 0; i < lines; i++)
-		{
-			GarbageGraphicsType graphic;
-			if(i == 0)
-			{
-				graphic = GARBAGE_GRAPHICS_TOP;
-			}
-			else if(i == lines - 1)
-			{
-				graphic = GARBAGE_GRAPHICS_BOTTOM;
-			}
-			else
-			{
-				graphic = GARBAGE_GRAPHICS_MIDDLE;
-			}
-			chunks[i]->SetGraphic(graphic);
-		}
-	}
+			chunks[i]->SetGraphic(GetGraphic(i, lines));
 }
 
 BaseBlock* GarbageBlock::GetBlock(int num)
 {
-	if(num >= numBlocks)
-		return (Garbage*)-1;
+	ASSERT(num >= 0 && num < numBlocks);
 
-	if(numBlocks > 6)
-		return chunks[num/6]->GetBlock(num%6);
-
-	return chunks[0]->GetBlock(num);
-}
-
-int GarbageBlock::GetNum()
-{
-	return numBlocks;
+	return chunks[num/6]->GetBlock(num%6);
 }
 
 void GarbageBlock::Drop()
@@ -146,9 +104,7 @@ void GarbageBlock::Drop()
 	if((numFalling == numBlocks || numFalling == 6) && state != BST_POP)
 	{
 		for(int i = 0;i < numBlocks;i++)
-		{
 			GetBlock(i)->ChangeState(BST_FALLING);
-		}
 		state = BST_FALLING;
 		numFalling = 0;
 	}
@@ -159,9 +115,7 @@ void GarbageBlock::Land()
 	if(state == BST_FALLING)
 	{
 		for(int i = 0;i < numBlocks;i++)
-		{
 			GetBlock(i)->ChangeState(BST_IDLE);
-		}
 		state = BST_IDLE;
 	}
 }
@@ -169,9 +123,7 @@ void GarbageBlock::Land()
 void GarbageBlock::Hover(int delay)
 {
 	for(int i = 0;i < numBlocks;i++)
-	{
 		GetBlock(i)->ChangeState(BST_IDLE);
-	}
 }
 
 void GarbageBlock::Tick()
@@ -179,21 +131,13 @@ void GarbageBlock::Tick()
 	if(state == BST_IDLE && numFalling != 0)
 	{
 		for(int i = 0;i < numBlocks;i++)
-		{
 			GetBlock(i)->ChangeState(BST_IDLE);
-		}
 	}
 	if(state == BST_POP && --popDelay <= 0)
 	{
-		//for(int i = 0;i < numBlocks;i++)
-		//{
-		///	GetBlock(i)->ChangeState(BST_FALLING);
-		//}
 		state = BST_FALLING;
 		for(int i = 0; i < lines-1; i++)
-		{
 			chunks[i]->SetChain(NULL);
-		}
 	}
 	numFalling = 0;
 }
